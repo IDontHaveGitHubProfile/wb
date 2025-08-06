5797e5d6c0fd85895fee5737d12947e56c55d545
IDontHaveGitHubProfile <160520731+IDontHaveGitHubProfile@users.noreply.github.com>
2025-08-06 12:00:58 +0400

    Update database.py

--- wb/app/database.py
+++ wb/app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASS = os.getenv("DB_PASS", "P4ssw0rd")
+DB_PASS = os.getenv("DB_PASS", "ваш_пароль")
 DB_NAME = os.getenv("DB_NAME", "wb_products")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
@@ -15,3 +15,4 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
